b8d75d3ad9003d13032f3d7b66e14a10ffa2e34c
fminor5 <fminor5@gmail.com>
2021-03-07 19:49:23 -0600

    First commint with first challenge added.

---
+++ 01-Fundamentals-Par-1/assignments.js
@@ -0,0 +1,40 @@
+/////////////////////////
+// Values and variables
+
+const country = "Mexico";
+const continent = "American";
+let population = 120000000;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+//////////////////////////
+// Data types.
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+///////////////////////////
+// let, const and var.
+
+language = "Spanish";
+
+
+///////////////////////////
+// Basic operators
+let halfOfPopulation = population / 2;
+console.log(halfOfPopulation);
+halfOfPopulation+=1
+console.log(halfOfPopulation);
+const Finland = 6000000;
+console.log(population > Finland);
+const averagePopulationInACountry = 33000000;
+console.log(population < averagePopulationInACountry);
+let description = "Portugal is in Europe, and its 11 million people speak portuguese."
---
+++ 01-Fundamentals-Par-1/script.js
@@ -0,0 +1,77 @@
+/*
+////////////////////////
+// Values and Variables
+
+let js = "amazing";
+if (js === "amazing") alert("JS is fun!");
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);*/
+
+///////////////////////
+// Data Types
+
+// let jsIsFun = true;
+// console.log(jsIsFun);
+
+// console.log(typeof true);
+// console.log(typeof jsIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1990;
+// console.log(year);
+// console.log(typeof year);
+
+///////////////////////////////
+// let, const and var
+
+
+///////////////////////////////
+// Basic Operators
+
+// Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+
+// // Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 400
+// x++; // x = x + 1
+// x--; // x = x - 1
+// x--;
+// console.log(x);
+
+// // Comparison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// Operator precendence
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah);
