0c539fb3da26f1cd7bb74f5ae7f524d5637c3196
Ndominor5 <reyesminorf@guilford.edu>
2021-03-07 20:25:27 -0600

    Adding module 17.

--- 01-Fundamentals-Par-1/assignments.js
+++ 01-Fundamentals-Par-1/assignments.js
@@ -1,9 +1,9 @@
 /////////////////////////
 // Values and variables
 
-const country = "Mexico";
-const continent = "American";
-let population = 120000000;
+// const country = "Mexico";
+// const continent = "American";
+// let population = 120000000;
 
 // console.log(country);
 // console.log(continent);
@@ -13,28 +13,36 @@ let population = 120000000;
 //////////////////////////
 // Data types.
 
-const isIsland = false;
-let language;
+// const isIsland = false;
+// let language;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
-///////////////////////////
-// let, const and var.
+// ///////////////////////////
+// // let, const and var.
 
-language = "Spanish";
+// language = "Spanish";
 
 
 ///////////////////////////
 // Basic operators
-let halfOfPopulation = population / 2;
-console.log(halfOfPopulation);
-halfOfPopulation+=1
-console.log(halfOfPopulation);
-const Finland = 6000000;
-console.log(population > Finland);
-const averagePopulationInACountry = 33000000;
-console.log(population < averagePopulationInACountry);
-let description = "Portugal is in Europe, and its 11 million people speak portuguese."
+// let halfOfPopulation = population / 2;
+// console.log(halfOfPopulation);
+// halfOfPopulation+=1
+// console.log(halfOfPopulation);
+// const Finland = 6000000;
+// console.log(population > Finland);
+// const averagePopulationInACountry = 33000000;
+// console.log(population < averagePopulationInACountry);
+// let description = "Portugal is in Europe, and its 11 million people speak portuguese."
+
+/////////////////////////////
+// String and Template Literals
+const description = `Portugal is in Europe,
+and its 11 million
+people speak portuguese`;
+
+console.log(description);
--- 01-Fundamentals-Par-1/script.js
+++ 01-Fundamentals-Par-1/script.js
@@ -64,14 +64,41 @@ console.log(40 + 8 + 23 - 10);*/
 
 // Operator precendence
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(now - 1991 > now - 2018);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(now - 1991 > now - 2018);
 
-let x, y;
-x = y = 25 - 10 - 5;
-console.log(x, y);
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
 
-const averageAge = (ageJonas + ageSarah) / 2;
-console.log(ageJonas, ageSarah);
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah);
+
+////////////////////////////
+// Strings and Template Literals
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+
+console.log(jonas);
+
+// Template literals
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Template literals can be use for any strings.`);
+
+// Multi lines
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String with
+multiple
+lines.`);
