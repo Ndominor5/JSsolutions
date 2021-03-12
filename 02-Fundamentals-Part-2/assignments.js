fb2a97bfc5efcbc85ee1ef0fe50dc36ea870e9e2
fminor5 <fminor5@gmail.com>
2021-03-12 08:05:42 -0600

    Adding modules 32-34.

---
+++ 02-Fundamentals-Part-2/assignments.js
@@ -0,0 +1,49 @@
+"use strict";
+
+///////////////////
+// Functions
+
+// function describeCountry(country, population, capitalCity) {
+//   return `${country}, has ${population} million people and its capital city is ${capitalCity}.`;
+// }
+
+// const country = prompt(`Enter a country:`);
+// const population = prompt(
+//   `Enter the population of the country that you previously entered:`
+// );
+// const capitalCity = prompt(`Enter the capital city:`);
+
+// const countryInformation = describeCountry(country, population, capitalCity);
+// console.log(countryInformation);
+
+/////////////////////
+// Function Declaration and Expressions
+
+// Function Declaration
+// function percentageOfWorld1(population) {
+//   return (population / 7900) * 100;
+// }
+
+// const worldPopulation1 = percentageOfWorld1(1441);
+// const worldPopulation2 = percentageOfWorld1(127);
+// const worldPopulation3 = percentageOfWorld1(337);
+
+// console.log(worldPopulation1);
+// console.log(worldPopulation2);
+// console.log(worldPopulation3);
+
+// Function Expression
+// const percentageOfWorld2 = function (population) {
+//   return (population / 7900) * 100;
+// };
+
+// const wP1 = percentageOfWorld2(1441);
+// const wP2 = percentageOfWorld2(127);
+// const wP3 = percentageOfWorld2(337);
+
+// console.log(wP1);
+// console.log(wP2);
+// console.log(wP3);
+
+//////////////////////
+// Arrow Functions
