6b906559aa02df102879783faed8ee4d6d5d239f
Ndominor5 <reyesminorf@guilford.edu>
2021-03-12 19:31:38 -0600

    Adding modules 35 and 36.

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -47,3 +47,34 @@
 
 //////////////////////
 // Arrow Functions
+
+// const percentageOfWorld3 = population => (population / 7900) * 100;
+
+// const chinaPopulation = percentageOfWorld3(1441);
+// const mexicoPopulation = percentageOfWorld3(127);
+// const usaPopulation = percentageOfWorld3(337);
+
+// console.log(chinaPopulation);
+// console.log(mexicoPopulation);
+// console.log(usaPopulation);
+
+////////////////////////
+// Functions Calling Other Functions
+// function worldPopuPercentage(countryPopulation){
+//     return ((countryPopulation / 7900) * 100).toFixed(2);
+// };
+
+// const descrivbePopulation = function(country, population){
+//     const worldPopulation = worldPopuPercentage(population);
+
+//     const message = `${country} has ${population} million people, which is about ${worldPopulation}% of the world.`
+
+//     console.log(message);
+// };
+
+// descrivbePopulation('china', 1441);
+// descrivbePopulation('mexico', 127);
+// descrivbePopulation('usa', 337);
+
+//////////////////////////
+// Introduction to Arrays
